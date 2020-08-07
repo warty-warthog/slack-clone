7bd7138e48aaf285e40c2a14aa1571b9e091848c
Quavo Huncho <sage@localhost.localdomain>
2020-08-07 18:40:26 +0000

    Added ability to display users that you can message directly

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
-import UserPanel from './UserPanel';
-import Channels from './Channels';
+import UserPanel from "./UserPanel";
+import Channels from "./Channels";
+import DirectMessages from "./DirectMessages";
 
 class SidePanel extends React.Component {
   render() {
-
     const { currentUser } = this.props;
 
     return (
@@ -16,11 +16,11 @@ class SidePanel extends React.Component {
         fixed="left"
         style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
       >
+        <UserPanel currentUser={currentUser} />
 
-          <UserPanel currentUser={currentUser} />
+        <Channels currentUser={currentUser} />
 
-          <Channels currentUser={currentUser} />
-
+        <DirectMessages currentUser={currentUser} />
       </Menu>
     );
   }
